e1f265e24cb00c437b9a95be7a78fe63d23b99cf
NeDEV <12139171+teneketo@users.noreply.github.com>
2021-03-02 10:35:54 +0000

    Update AUTOMATIC_HEATER_CONTROL_WITH_DHT11_TEMPERATURE_SENSOR.ino

--- AUTOMATIC_HEATER_CONTROL_WITH_DHT11_TEMPERATURE_SENSOR/AUTOMATIC_HEATER_CONTROL_WITH_DHT11_TEMPERATURE_SENSOR.ino
+++ AUTOMATIC_HEATER_CONTROL_WITH_DHT11_TEMPERATURE_SENSOR/AUTOMATIC_HEATER_CONTROL_WITH_DHT11_TEMPERATURE_SENSOR.ino
@@ -43,7 +43,7 @@ void get_temp_and_hum(){
   
   temperature[i]=(DHT.temperature);
   humidity[i]=(DHT.humidity);
-  delay(1000);
+  delay(1000); //AKo iska6 da se obnowqwa wsqka sekunda a ne na wseki 10 sekundi naprawi towa da e 100ms a ne 1000ms (1s)
   }
 
 
@@ -70,6 +70,14 @@ void heater_control(){
   else digitalWrite(relay_pin,LOW);
 }
 
+//Nowata funkciq
+//Raboti po identi4en na4in kato temperaturata:
+//Koda po-gore 10 pyti, 1 pyt w sekunda wzima temperaturata i wlajnostta, sled towa iz4islqwa srednata
+void vlajnost_control(){
+  if (avg_hum<70)digitalWrite(relay_pin,HIGH); //AKo vjlajnostta e nad 70, swetni sy6toto ne6to kato temperaturata, tuka 6te si sloji6 kakwoto ti trqbwa
+  else digitalWrite(relay_pin,LOW);
+}
+
 
 
 void Display(){
@@ -87,6 +95,7 @@ lcd.print("Humidity = ");
 lcd.print(avg_hum);   
 lcd.print("%"); 
 heater_control();  
+vlajnost_control();
   }
 
 void loop(){
